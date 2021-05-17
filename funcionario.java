997418e81e70024bd76c7aae18d44c7a67f12ffa
Isaifo <ysoca98@hotmail.com>
2021-05-17 20:00:04 -0300

    Diferentes classes referente a um sistema de biblioteca

---
+++ funcionario.java
@@ -0,0 +1,19 @@
+package Teste;
+
+public class funcionario extends pessoa{
+	
+
+	public funcionario(String nome, String email, int telefone, int idade, int cod)
+	{
+		super(nome, email, telefone, idade, cod);
+	}
+	
+	public String getInfo()
+	{
+		return "\nNome do funcionário: " + super.getNome() + "\nEmail: " + super.getEmail() + "\nIdade: " 
+		+ super.getIdade() + "\nTelefone: " + super.getTelefone() + "\nCódigo do funcionário: " + super.getCod() ;
+		
+	}
+	
+}
+
